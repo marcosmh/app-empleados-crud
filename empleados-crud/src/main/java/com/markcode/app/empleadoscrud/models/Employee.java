7014d6acb999b1981949cb3df84600cd6d080ed3
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2023-03-09 17:34:33 -0600

    modificacion Employee

--- empleados-crud/src/main/java/com/markcode/app/empleadoscrud/models/Employee.java
+++ empleados-crud/src/main/java/com/markcode/app/empleadoscrud/models/Employee.java
@@ -9,7 +9,7 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="firts_name")
+    @Column(name="firstName")
     private String firstName;
 
     @Column(name="last_name")
